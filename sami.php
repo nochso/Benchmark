7d9d557925ff3ad0ce3a91291c2e0f2ffc8792b9
Marcel Voigt <mv@noch.so>
2015-09-13 00:55:27 +0200

    Apply comment headers

--- sami.php
+++ sami.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of nochso/benchmark.
+ *
+ * @copyright Copyright (c) 2015 Marcel Voigt <mv@noch.so>
+ * @license   https://github.com/nochso/benchmark/blob/master/LICENSE ISC
+ * @link      https://github.com/nochso/benchmark
+ */
+
 return new Sami\Sami('src', array(
     'title' => 'nochso/Benchmark documentation',
     'build_dir' => __DIR__ . '/doc/build',
--- src/Report.php
+++ src/Report.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of nochso/benchmark.
+ *
+ * @copyright Copyright (c) 2015 Marcel Voigt <mv@noch.so>
+ * @license   https://github.com/nochso/benchmark/blob/master/LICENSE ISC
+ * @link      https://github.com/nochso/benchmark
+ */
+
 namespace nochso\Benchmark;
 
 use Cocur\Slugify\Bridge\Twig\SlugifyExtension;
