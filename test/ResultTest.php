f0491e9305a67702b4ed4c5931219b1a1476d4a9
Marcel Voigt <mv@noch.so>
2016-04-17 20:11:46 +0200

    Switch tests over to normalized operations per unit

--- test/ResultTest.php
+++ test/ResultTest.php
@@ -13,6 +13,7 @@ namespace nochso\Benchmark\Test;
 use nochso\Benchmark\Method;
 use nochso\Benchmark\Parameter;
 use nochso\Benchmark\Result;
+use nochso\Benchmark\UnitResult;
 
 class ResultTest extends \PHPUnit_Framework_TestCase
 {
@@ -26,25 +27,27 @@ class ResultTest extends \PHPUnit_Framework_TestCase
     public function testToString($duration, $ops, $expected)
     {
         $result = new Result($duration, $ops, new Method(null, ''), new Parameter(null, ''));
-        $this->assertEquals($expected, (string) $result);
+        $ur = new UnitResult();
+        $ur->add($result);
+        $this->assertEquals($expected, $result->getNormalizedOperationsPerUnit($ur));
     }
 
     public function toStringProvider()
     {
         return array(
-            array(100.0, 1, '10.0 op/s'),
-            array(2000.0, 1, '30.0 op/m'),
-            array(4000.0, 1, '15.0 op/m'),
-            array(60*1000, 1, '1.0 op/m'),
-            array(60*60*1000, 1, '1.0 op/h'),
-            array(1000.0, 1, '1.0 op/s'),
-            array(1000.0, 5, '5.0 op/s'),
-            array(2000.0, 10, '5.0 op/s'),
-            array(1000.0, 1001, '1.0K op/s'),
-            array(1000.0, 1200, '1.2K op/s'),
-            array(1000.0, 1200000, '1.2M op/s'),
-            array(1000.0, 1200000000, '1.2G op/s'),
-            array(1000.0, 1200000000000, '1.2T op/s'),
+            array(100.0, 1, '10.0/s'),
+            array(2000.0, 1, '30.0/m'),
+            array(4000.0, 1, '15.0/m'),
+            array(60*1000, 1, '1.0/m'),
+            array(60*60*1000, 1, '1.0/h'),
+            array(1000.0, 1, '1.0/s'),
+            array(1000.0, 5, '5.0/s'),
+            array(2000.0, 10, '5.0/s'),
+            array(1000.0, 1001, '1.0K/s'),
+            array(1000.0, 1200, '1.2K/s'),
+            array(1000.0, 1200000, '1.2M/s'),
+            array(1000.0, 1200000000, '1.2G/s'),
+            array(1000.0, 1200000000000, '1.2T/s'),
         );
     }
 }
